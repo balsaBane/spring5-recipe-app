6920a6a132a727d75eb51a947b84816d83fa0c56
Balsa <balsa.bane@gmail.com>
2019-02-06 12:41:30 +0100

    Create Recipe and Notes entities

---
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,33 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private String recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public String getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(String recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}

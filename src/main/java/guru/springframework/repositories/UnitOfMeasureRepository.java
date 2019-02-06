b7b2d4cbb3f3556ebace2d6105ab7b7610ffbbb3
Balsa <balsa.bane@gmail.com>
2019-02-06 14:18:28 +0100

    Create Spring data repositories

---
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,10 @@
+package guru.springframework.repositories;
+
+import guru.springframework.domain.UnitOfMesure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMesure, Long>{
+
+
+
+}
